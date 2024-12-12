f7dae997648b287ae09e5d7acf83870b470e62f0
John Park <kyopark2014@gmail.com>
2024-12-12 17:27:34 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -99,7 +99,7 @@ multi_region_models = [   # Nova Pro
     {
         "bedrock_region": "us-east-2", # Ohio
         "model_type": "nova",
-        "model_id": "amazon.nova-pro-v1:0"
+        "model_id": "us.amazon.nova-pro-v1:0"
     },
     {
         "bedrock_region": "us-west-2", # Oregon
